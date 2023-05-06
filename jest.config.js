618968050f270ca29ec4e8990cca2a6453899d04
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-05-06 20:55:22 -0300

    chore: add jest, script test and setup .lintstageddr.js

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  clearMocks: true,
+  coverageProvider: 'v8'
+};
