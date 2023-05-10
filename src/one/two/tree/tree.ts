871a5e0e4736de65af03ed0cc5edf07a3b98ca28
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-05-10 15:36:08 -0300

    chore: move index.spec.ts to directory test

---
+++ src/one/two/tree/tree.ts
@@ -0,0 +1,4 @@
+import Person from '@/index';
+
+const eu = new Person();
+console.log(eu.sayName());
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,14 @@
-import Person from '.';
+import Person from '@/index';
 
 describe('teste operacoes aritmeticas', () => {
   test('testa soma', () => {
-    const person = new Person();
     expect(1 + 2).toBe(3);
+  });
+});
+
+describe('teste operacoes Pessoa', () => {
+  test('testa Pessoa', () => {
+    const person = new Person();
     expect(person.sayName()).toBe('Skinner');
   });
 });
